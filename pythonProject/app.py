c172eb0e874181d42198a2deaec4acca1e614454
Sang <tungsang.working@gmail.com>
2023-12-25 22:37:22 +0800

    init python project

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -59,20 +59,20 @@
 # # else:
 # #     print("You win")
 
-# command = ''
-# while command.lower() != 'quit':
-#     command = input("> ").lower()
-#     if command == 'start':
-#         print('Car started...Ready to go!')
-#     elif command == 'stop':
-#         print('Car stopped.')
-#     elif command == 'help':
-#         print('''
-#             start - to start the car
-#             stop - to stop the car
-#             quit - to exit
-#                   ''')
-#     elif command == 'quit':
-#         break
-#     else:
-#         print('I dont understand that...')
+command = ''
+while command.lower() != 'quit':
+    command = input("> ").lower()
+    if command == 'start':
+        print('Car started...Ready to go!')
+    elif command == 'stop':
+        print('Car stopped.')
+    elif command == 'help':
+        print('''
+            start - to start the car
+            stop - to stop the car
+            quit - to exit
+                  ''')
+    elif command == 'quit':
+        break
+    else:
+        print('I dont understand that...')
